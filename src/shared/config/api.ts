efa0f8f23d96a4eddb2dc0cf80ac79a95660ed30
artfourteen <shadiyarovarmat@gmail.com>
2026-02-09 15:11:30 +0500

    core: base file structure

---
+++ src/shared/config/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import { apiUrl } from "@/shared/config/env";
+
+export const api = axios.create({
+  baseURL: `${apiUrl}/api/v1`,
+  withCredentials: true,
+  withXSRFToken: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
