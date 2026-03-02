08edbe638ff90ebcbc54ea657b3031090ac13b7c
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2026-03-02 14:37:28 +0500

    feat: add id for features and how it works sections

--- src/widgets/landing-page/ui/features-section.tsx
+++ src/widgets/landing-page/ui/features-section.tsx
@@ -7,7 +7,10 @@ import { SectionHeader } from "./section-header";
 
 export const FeaturesSection = () => {
   return (
-    <section className="py-20 sm:py-32 container mx-auto px-4 mt-4 space-y-18">
+    <section
+      id="features"
+      className="py-20 sm:py-32 container mx-auto px-4 mt-4 space-y-18 scroll-mt-2 lg:-scroll-mt-10"
+    >
       <SectionHeader
         title="That Makes Easy Time Tracking"
         description="Track productivity, attendance with simple time tracker and timesheets to make your team's work easier."
--- src/widgets/landing-page/ui/how-we-work-section.tsx
+++ src/widgets/landing-page/ui/how-we-work-section.tsx
@@ -9,7 +9,10 @@ import { SectionHeader } from "./section-header";
 
 export const HowWeWorkSection = () => {
   return (
-    <section className="sm:py-12 sm:px-4">
+    <section
+      id="how-it-works"
+      className="sm:py-12 sm:px-4 scroll-mt-15 lg:scroll-mt-10"
+    >
       <div className="relative container space-y-24 mx-auto px-4 py-12 sm:rounded-lg bg-linear-to-br from-primary/6 via-primary/3 to-primary/7">
         <div className="absolute left-0 top-30 pointer-events-none -z-10 h-100 w-100 rounded-full bg-linear-to-br from-primary/40 via-purple-400/30 to-indigo-500/20 opacity-40 blur-[100px]" />
         <div className="absolute right-0 bottom-30 pointer-events-none -z-10 h-100 w-100 rounded-full bg-linear-to-br from-primary/40 via-purple-400/30 to-indigo-500/20 opacity-40 blur-[100px]" />
