9b4ecf3965aed8271508791a7befe279eaa2ded1
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2026-03-03 09:21:44 +0500

    feat: add reviews section on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,19 @@ import {
   FeaturesSection,
   HeroSection,
   HowWeWorkSection,
+  ReviewsSection,
 } from "@/widgets/landing-page";
 import { SiteShell } from "@/widgets/site/ui/site-shell";
 
 export default function Home() {
   return (
     <SiteShell>
-      <main>
+      <main className="overflow-x-hidden">
         <HeroSection />
         <FeaturesSection />
         <HowWeWorkSection />
         <BenefitsCardsSection />
+        <ReviewsSection />
         {/*<StatsSection />*/}
         {/*<BenefitSection />*/}
         {/*<CTASection />*/}
---
+++ src/widgets/landing-page/ui/review-card.tsx
@@ -0,0 +1,36 @@
+import { Star } from "lucide-react";
+import { UserAvatar } from "@/entities/user";
+import { Card, CardContent, CardFooter, CardHeader } from "@/shared/ui/card";
+import type { Review } from "../consts";
+
+interface ReviewCardProps {
+  review: Review;
+}
+
+export const ReviewCard = ({ review }: ReviewCardProps) => {
+  return (
+    <Card className="h-full min-w-60 max-w-75 sm:max-w-100 shrink-0 select-none hover:bg-gray-50 active:bg-gray-50 transition-colors duration-75 gap-4">
+      <CardHeader className="gap-0">
+        <div className="flex items-center gap-1">
+          {Array.from({ length: review.rating }, (_, i) => i).map((i) => (
+            <Star key={i} className="size-4 fill-yellow-500 text-yellow-500" />
+          ))}
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm text-muted-foreground line-clamp-4">
+          {review.review}
+        </p>
+      </CardContent>
+      <CardFooter className="flex gap-3 items-center mt-auto pt-2">
+        <UserAvatar name={review.name} />
+        <div>
+          <h2 className="text-sm font-medium">{review.name}</h2>
+          <p className="text-sm text-muted-foreground">
+            {review.company} \ {review.role}
+          </p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
