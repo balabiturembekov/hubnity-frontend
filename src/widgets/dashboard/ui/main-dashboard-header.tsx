be9a94af5a14569a7d5f792d53bca195c0e68b53
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2026-03-05 08:52:29 +0500

    fix: remove skeleton for dashboard page header

--- src/widgets/dashboard/ui/main-dashboard-header.tsx
+++ src/widgets/dashboard/ui/main-dashboard-header.tsx
@@ -6,30 +6,14 @@ import Link from "next/link";
 import { useUser } from "@/entities/user";
 import { greeting } from "@/shared/lib/utils";
 import { Button } from "@/shared/ui/button";
-import { Skeleton } from "@/shared/ui/skeleton";
 import { DashboardPageHeader } from "@/widgets/header";
 
 export const MainDashboardHeader = () => {
-  const { user, isAdmin, isPending } = useUser();
-
-  if (!user || isPending) {
-    return (
-      <div className="flex items-center justify-between bg-white border-b p-6">
-        <div className="flex flex-col gap-2">
-          <Skeleton className="w-90 h-9" />
-          <Skeleton className="w-45 h-4" />
-        </div>
-        <div className="flex items-center gap-2">
-          <Skeleton className="w-40 h-9" />
-          <Skeleton className="w-40 h-9" />
-        </div>
-      </div>
-    );
-  }
+  const { user, isAdmin } = useUser();
 
   return (
     <DashboardPageHeader
-      title={`${greeting()}, ${user.name}! 👋`}
+      title={`${greeting()}, ${user?.name}! 👋`}
       subTitle={format(new Date(), "EEEE, MMMM d, yyyy")}
     >
       <Link href="/dashboard/tracking">
